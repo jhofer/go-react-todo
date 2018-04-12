d5f54148d506af2b13d30f32a8104a1f26466f37
jhofer <jonas.hofer@outlook.com>
2018-04-12 08:14:27 +0200

    fix script to work with first call

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
-cd ./client && yarn start & docker-compose -f docker-compose.dev.yml up && fg
+cd ./client && yarn install && yarn start & docker-compose -f docker-compose.dev.yml build
+ && docker-compose -f docker-compose.dev.yml up && fg
